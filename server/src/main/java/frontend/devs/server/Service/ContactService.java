ad1614ff4918fca7bfda4c1e7acd887964e7d7b8
NV-Phong <0h9ng03@gmail.com>
2024-07-10 07:47:39 +0700

    [ API ] Product & Contact

--- server/src/main/java/frontend/devs/server/Service/ContactService.java
+++ server/src/main/java/frontend/devs/server/Service/ContactService.java
@@ -1,21 +1,23 @@
 package frontend.devs.server.Service;
 
 import frontend.devs.server.Entities.Collection;
+import frontend.devs.server.Entities.Contact;
 import frontend.devs.server.Entities.User;
-import frontend.devs.server.Repositories.CollectionRepository;
+import frontend.devs.server.Repositories.ContactRepository;
 import frontend.devs.server.Repositories.UserRepository;
 import frontend.devs.server.Security.JwtAuthenticationFilter;
 import frontend.devs.server.Security.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class CollectionService
+public class ContactService
 {
 	@Autowired
-	private CollectionRepository collectionRepository;
+	private ContactRepository       contactRepository;
 
 	@Autowired
 	private JwtAuthenticationFilter _JwtAuthenticationFilter;
@@ -26,35 +28,31 @@ public class CollectionService
 	@Autowired
 	private UserRepository _UserRepository;
 
-	public List<Collection> findAll()
-		{
-			return collectionRepository.findAll();
-		}
-
-	public List<Collection> findByID(HttpServletRequest request)
+	public List<Contact> findContactbyIDUser(HttpServletRequest request)
 		{
 			String token = _JwtAuthenticationFilter.getJwtFromRequest(request);
 			if (token != null && _JwtTokenProvider.validateToken(token))
 				{
 					String userId = _JwtTokenProvider.getUserIdFromToken(token);
-					return collectionRepository.findByIDUser(userId);
+					return contactRepository.findByIDUser(userId);
 				}
 			return null;
-
 		}
 
-
-	public Collection CreateCollection(Collection collection, HttpServletRequest request)
+	public Contact CreateContact(Contact contact, HttpServletRequest request)
 		{
 			String token = _JwtAuthenticationFilter.getJwtFromRequest(request);
 			if (token != null && _JwtTokenProvider.validateToken(token))
 				{
-					String         userId = _JwtTokenProvider.getUserIdFromToken(token);
-					User user = _UserRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-					collection.setUser(user);
+					String userId = _JwtTokenProvider.getUserIdFromToken(token);
+					User   user   = _UserRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+					contact.setUser(user);
 				}
 
-			return collectionRepository.save(collection);
+			return contactRepository.save(contact);
 		}
-
+	public List<Contact> findAll()
+		{
+			return contactRepository.findAll();
+		}
 }
