3968fc01e6efa9570acec65d62b67fa22074f1ff
NV-Phong <0h9ng03@gmail.com>
2024-07-11 02:20:16 +0700

    [ Add Product To Cart ]

--- server/src/main/java/frontend/devs/server/Repositories/ProductRepository.java
+++ server/src/main/java/frontend/devs/server/Repositories/ProductRepository.java
@@ -8,14 +8,20 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String>
 {
+
+	Optional<Product> findById(String id);
+
 	@Query("SELECT u FROM Product u WHERE u.user.IDUser = :IDUser")
 	List<Product> findByIDUser(@Param("IDUser") String IDUser);
 
 	@Query("SELECT u FROM Product u WHERE u.collection.id = :id")
 	List<Product> findByIDCollection(@Param("id") String id);
+
+
 
 }
